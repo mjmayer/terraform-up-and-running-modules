82f69ccf7081b7d30c08ae2a90c5f30e211b8f06
Michael Mayer <mjmayer@ucdavis.edu>
2021-09-29 13:34:03 -0700

    Add dynamic tags

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -59,7 +59,15 @@ resource "aws_autoscaling_group" "example" {
     value               = "${var.cluster_name}-terraform-asg-example"
     propagate_at_launch = true
   }
+  dynamic "tag" {
+    for_each = var.custom_tags
 
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
 }
 
 resource "aws_security_group" "instance" {
