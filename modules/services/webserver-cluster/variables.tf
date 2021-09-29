528d2c197b3c9692bb4653274fa8bbd1bc0cbc8a
Michael Mayer <mjmayer@ucdavis.edu>
2021-09-29 11:53:07 -0700

    Add custom tags to webserver

--- modules/services/webserver-cluster/variables.tf
+++ modules/services/webserver-cluster/variables.tf
@@ -28,3 +28,9 @@ variable "max_size" {
   description = "The maximum number of EC2 instances in the ASG"
   type        = number
 }
+
+variable "custom_tags" {
+  description = "Custom tags to set on the instances in the ASG"
+  type        = map(string)
+  default     = {}
+}
