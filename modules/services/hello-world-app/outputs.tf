755dae2a3c537ee2ec1275dac75237644eff5662
Michael Mayer <mjmayer@ucdavis.edu>
2021-10-01 13:20:05 -0700

    Remove duplicate outputs

--- modules/services/hello-world-app/outputs.tf
+++ modules/services/hello-world-app/outputs.tf
@@ -1,31 +1,16 @@
-
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
-
-output "asg_name" {
-  value       = aws_autoscaling_group.example.name
-  description = "The name of the Auto Scaling Group"
-}
-
 output "alb_security_group_id" {
   value       = aws_security_group.alb.id
   description = "The ID of the Security Group attached to the load balancer"
-
 }
-
 output "alb_dns_name" {
-  value = module.alb.alb_dns_name
+  value       = module.alb.alb_dns_name
   description = "The domain name of the load balancer"
 }
-
 output "asg_name" {
-  value = module.asg.asg_name
+  value       = module.asg.asg_name
   description = "The name of the Auto Scaling Group"
 }
-
 output "instance_security_group_id" {
-  value = module.asg.instance_security_group_id
+  value       = module.asg.instance_security_group_id
   description = "The ID of the EC2 Instance Security Group"
 }
