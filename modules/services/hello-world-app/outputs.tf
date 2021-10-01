182603556b6a8036ebe0c18032f69a8fe2bc70ab
Michael Mayer <mjmayer@ucdavis.edu>
2021-10-01 14:08:00 -0700

    Fix object reference

--- modules/services/hello-world-app/outputs.tf
+++ modules/services/hello-world-app/outputs.tf
@@ -1,5 +1,5 @@
 output "alb_security_group_id" {
-  value       = module.alb.dns_name
+  value       = module.alb.alb_security_group_id
   description = "The ID of the Security Group attached to the load balancer"
 }
 output "alb_dns_name" {
